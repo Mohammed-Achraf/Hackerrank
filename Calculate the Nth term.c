5be871d9291bd72fc0cd6b56598b95e4c4c12ac7
Mohammed-Achraf <168601242+Mohammed-Achraf@users.noreply.github.com>
2024-06-16 07:25:36 +0200

    Create Calculate the Nth term.c

---
+++ Calculate the Nth term.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+int find_nth_term(int n, int a, int b, int c) {
+  //Write your code here.
+  if(n > 3)
+  {
+      a = a + b + c;
+      b = a + b + c;
+      c = a + b + c;
+      return find_nth_term(n - 3, a, b, c);
+  }
+  if ( n == 1)
+    return a;
+  else if ( n == 2)
+    return b;
+  else if (n == 3)
+    return c;
+return 0;
+}
+
+int main() {
+    int n, a, b, c;
+  
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+ 
+    printf("%d", ans); 
+    return 0;
+}
